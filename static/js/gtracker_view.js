1a46ed881a2e68c35d8c9c16830a7905e5afc085
Brad Smith <brad.alan.smith@gmail.com>
2016-07-03 14:27:34 -0700

    map!

--- static/js/gtracker_view.js
+++ static/js/gtracker_view.js
@@ -37,4 +37,66 @@ var rows = get_rows(),
     chart.load({
         rows: rows
     })
+    add_rows(rows)
 })
+
+var map = L.map(
+    'map', {}
+).setView([36, 139],4);
+
+L.tileLayer(
+    'https://otile1-s.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png'
+).addTo(map);
+
+function add_point(item, display_layer) {
+    var default_style = {
+            color: 'red',
+            fillColor: 'blue',
+            radius: 10,
+            weight: 2,
+            fillOpacity: 0.5,
+        },
+        label = '<span>' + item.obstime + '</span>'
+        mk = L.circleMarker([item.lat, item.lng], default_style)
+            .addTo(display_layer);
+    
+    mk.bindPopup(
+        label, {
+            offset: L.point(0,-10),
+            className: 'custom-popup'
+        }
+    )
+        .on('mouseover', function show_tooltip () { this.openPopup(); })
+        .on('mouseout', function hide_tooltip () { this.closePopup(); })
+        .on('click', function map_click (e) {
+            try {
+                self.last_marker.setStyle(default_style)
+            } catch (e) {}
+            e.target.setStyle({
+                color: 'orange',
+                fillColor: 'green',
+                radius: 15,
+                weight: 2,
+                fillOpacity: 0.5
+            });
+            self.last_marker = e.target;
+            router.navigate('photos/' + item.id, true);
+        });
+
+    return mk;
+}
+
+function add_rows (rows) {
+    try {
+        map.removeLayer(display_layer);
+    } catch (e) {  }
+    display_layer = L.markerClusterGroup()
+    
+    rows.forEach(function(d) {
+        add_point(d, display_layer)
+    })
+    
+    display_layer.addTo(map);
+}
+
+add_rows(rows)
