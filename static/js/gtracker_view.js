cd39c733b0d60860882aacda870ebd8ce3d4106e
Brad Smith <brad.alan.smith@gmail.com>
2016-07-03 14:00:58 -0700

    chart update automatically?

--- static/js/gtracker_view.js
+++ static/js/gtracker_view.js
@@ -1,28 +1,40 @@
 
-var item_template = $('#item_template').text(),
-    rows = [["x", "ping", "download", "upload"]]
+var item_template = $('#item_template').text()
+    
 
-Object.keys(localStorage).sort().forEach(function(d) {
-    var item = JSON.parse(localStorage[d])
-    if (item.ping > 500) return
-    rows.push([Date.parse(item.obstime), item.ping, item.download, item.upload])
-})
+function get_rows() {
+    var rows = [["x", "ping", "download", "upload"]]
+    Object.keys(localStorage).sort().forEach(function(d) {
+        var item = JSON.parse(localStorage[d])
+        if (item.ping > 500) return
+        rows.push([Date.parse(item.obstime), item.ping, item.download, item.upload])
+    })
+    return rows
+}
 
-var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-        x: 'x',
-        rows: rows
-    },
-    zoom: {
-        enabled: true
-    },
-    axis: {
-        x: {
-            type: 'timeseries',
-            tick: {
-                format: '%Y-%m-%d-%H-%M-%S'
+var rows = get_rows(),
+    chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            x: 'x',
+            rows: rows
+        },
+        zoom: {
+            enabled: true
+        },
+        axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    format: '%Y-%m-%d-%H-%M-%S'
+                }
             }
         }
-    }
+    })
+
+ $(window).bind('storage', function (e) {
+    rows = get_rows()
+    chart.load({
+        rows: rows
+    })
 })
